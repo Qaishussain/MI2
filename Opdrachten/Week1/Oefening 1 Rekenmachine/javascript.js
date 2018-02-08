014d255d8cd02669bd42adbb2d0751ce9f92167a
Qaishussain <32386896+Qaishussain@users.noreply.github.com>
2018-02-08 13:10:15 +0100

    laatste versie

--- Opdrachten/Week1/Oefening 1 Rekenmachine/javascript.js
+++ Opdrachten/Week1/Oefening 1 Rekenmachine/javascript.js
@@ -1,12 +1,33 @@
 "use strict";
-
-var x = 1;
-var y = 2;
+/*global document*/
+var x = 20;
+var y = 15;
 var total;
 optellen(x,y);
 function optellen(x,y){
 	total= x + y;
 	return total;
 }
-document.write(total);
 
+document.write("<p>Optellen(+): </p>", total)
+total = 0;
+af(x,y);
+function af(x,y){
+	total= x - y;
+	return total;
+}
+document.write("<p>Aftrekken(-):</p>",total)
+total = 0;
+maal(x,y);
+function maal(x,y){
+	total= x * y;
+	return total;
+}
+document.write("<p>Vermenigvuldigen(*):</p>",total)
+total = 0;
+delen(x,y);
+function delen(x,y){
+	total= x / y;
+	return total;
+}
+document.write("<p>Delen(/):</p>",total)
