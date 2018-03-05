8fb73c36e35fe142225912e7502c9bb889f50278
Qaishussain <32386896+Qaishussain@users.noreply.github.com>
2018-03-05 15:22:00 +0100

    Being Nice

--- Opdrachten/Week3/HideElementButton/javascript.js
+++ Opdrachten/Week3/HideElementButton/javascript.js
@@ -6,9 +6,11 @@ function myFunction() {
     if (img.style.display === "block") {
         img.style.display = "none";
 		 document.getElementById("btn").innerText  = "Show";
+		
     } else {
 		 document.getElementById("btn").innerText  = "Hide";
         img.style.display = "block";
     }
 	
 }
+
